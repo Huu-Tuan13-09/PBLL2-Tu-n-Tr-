efbf1682332d5ad497a56ace3204c83e20790e5c
Admin <truongtrivn123@gmail.com>
2024-09-20 10:50:48 +0700

    c

--- customer.h
+++ customer.h
@@ -9,41 +9,18 @@ private:
     string loyaltyLevel;
     string sex;
     int age;
-public: 
+public:
     Customer(){}
-
     Customer(string adress, string loyalLevel, string sex, int age) : customerId("C"+to_string(userId)), adress(adress), sex(sex), age(age){}
 
-    string setLoyaltyLevel(string level){
-        loyaltyLevel=level;
-    }
+    void setLoyaltyLevel(string level){loyaltyLevel=level;}
+    void setAdress(string ad){adress=ad;}
+    void setAge(int age){this->age=age;}
 
-    string setAdress(string ad){
-        adress=ad;
-    }
-
-    string getLoyaltyLevel(){
-        return loyaltyLevel;
-    }
-
-    string getCustomerId(){
-        return customerId;
-    }
-
-    string getAdress(){
-        return adress;
-    }
-
-    void setAge(int age){
-        this->age=age;
-    }
-
-    string getSex(){
-        return sex;
-    }
-
-    int getAge(){
-        return age;
-    }
+    string getLoyaltyLevel(){return loyaltyLevel;}
+    string getCustomerId(){return customerId;}
+    string getAdress(){return adress;}
+    int getAge(){return age;}
+    string getSex(){return sex;}
 
 };
