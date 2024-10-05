009eaab8ba2233f7844b99c948d9439599ff01a4
Admin <truongtrivn123@gmail.com>
2024-10-05 10:14:26 +0700

    aa

--- manager.h
+++ manager.h
@@ -26,9 +26,17 @@ public:
         }
     }
     //Theo doi lich lam viec
-    
+    void showSchedule(){
+        for(Barber barber : barbers){
+            vector<pair<string,vector<string>>> schedule = barber.getWorking();
+            // Trình bày tuỳ chỉnh
+        }
+    }
     //Dieu chinh lich lam viec cua tat ca cac tho
+    void adjustSchedule(){
+        showSchedule();
 
+    }
     //Xem bao cao, thong ke dich vu khach hang
 
     //Cap nhat muc do than thiet cua khach hang
